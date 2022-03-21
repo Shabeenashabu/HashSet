52e5b9b2356aa0df25c399bb33cea16bbb35ffc9
Shabeenashabu <102009729+Shabeenashabu@users.noreply.github.com>
2022-03-21 11:18:13 +0530

    Add files via upload

---
+++ Hashset1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Hashset1
+{
+public static void main(String[] args)
+{
+HashSet hs=new HashSet();
+hs.add("shabeena");
+hs.add("shaik");
+hs.add("kanna");
+hs.add("bujji");
+hs.add("chinna");
+hs.add(10);
+hs.add(20);
+hs.add(30);
+hs.add(40);
+hs.add(50);
+System.out.println(hs);
+}
+}
